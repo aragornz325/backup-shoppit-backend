4e8ee210670b056408467a9b83a43dfc8b38f2ba
JoakoV3 <58125197+JoakoV3@users.noreply.github.com>
2022-05-06 17:00:42 -0300

    get product by id

--- functions/src/products/products.controller.ts
+++ functions/src/products/products.controller.ts
@@ -3,7 +3,7 @@ import {db} from "../config/firebase";
 
 export async function getAll(req: Request, res: Response) {
   try {
-    const productsArray:Array<FirebaseFirestore.DocumentData> = [];
+    const productsArray: Array<FirebaseFirestore.DocumentData> = [];
     const products = await db.collection("products").get();
     products.docs.map((prod) => {
       productsArray.push(prod.data());
@@ -13,6 +13,21 @@ export async function getAll(req: Request, res: Response) {
     handleError(res, error);
   }
 }
+
+export const getProduct = async (req: Request, res: Response) => {
+  try {
+    const {id} = req.params;
+    if (!id) return res.status(500).send("Bad request");
+    const product = await db.collection("products").doc(id).get();
+    if (product.exists) {
+      return res.send(product.data());
+    } else {
+      return res.status(404).send("Not found");
+    }
+  } catch (error) {
+    return handleError(res, error);
+  }
+};
 
 function handleError(res: Response, err: any) {
   return res.status(500).send({message: `${err.code} - ${err.message}`});
--- functions/src/products/products.routes.ts
+++ functions/src/products/products.routes.ts
@@ -1,6 +1,7 @@
 import {Application} from "express";
-import {getAll} from "./products.controller";
+import {getAll, getProduct} from "./products.controller";
 
 export const productsRoutes = (app: Application) => {
   app.get("/products", getAll);
+  app.get("/products/:id", getProduct);
 };
