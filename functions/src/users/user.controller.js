8275b84200c99df3748abc15a6d1459a4e38cc3d
JoakoV3 <58125197+JoakoV3@users.noreply.github.com>
2022-05-11 19:36:53 -0300

    auth

--- functions/src/users/user.controller.js
+++ functions/src/users/user.controller.js
@@ -1,10 +1,11 @@
-const admin = require("firebase-admin");
 const { getAuth } = require("firebase-admin/auth")
 
 const tryUser = async (req, res) => {
-  await getAuth().setCustomUserClaims("p3Tn1lnvaTN8RaxYa940XXzVNso2", { user: true });
-  const userRecord = await admin.auth().getUser("p3Tn1lnvaTN8RaxYa940XXzVNso2");
-  res.json({data: userRecord.customClaims});
+  const auth = getAuth()
+
+  await auth.setCustomUserClaims("p3Tn1lnvaTN8RaxYa940XXzVNso2", { user: true });
+  const userRecord = await auth.getUser("p3Tn1lnvaTN8RaxYa940XXzVNso2");
+  res.json({ data: userRecord.customClaims });
 
 }
 
