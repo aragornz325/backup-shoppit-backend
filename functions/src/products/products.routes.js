feb206d4f2be1e23df3bbad32990bbb4442a3f51
Joaquin Varela <58125197+JoakoV3@users.noreply.github.com>
2022-05-17 17:51:25 -0300

    🏗 Fixed building error

--- functions/src/products/products.routes.js
+++ functions/src/products/products.routes.js
@@ -1,12 +1,12 @@
 const {
-  getAll, getProduct, AddProduct, updateProductCon,
+  getAll, getProduct, addProduct, updateProductCon,
 } = require('./products.controller');
 const validatorHandler = require('../middlewares/validatorHandler');
 const { createProduct, updateProduct, getOne } = require('../schemas/prod.schema'); /* DTOs */
 
 const productsRoutes = (app) => {
   app.get('/products', getAll);
-  app.post('/products', validatorHandler(createProduct, 'body'), AddProduct);
+  app.post('/products', validatorHandler(createProduct, 'body'), addProduct);
   app.get('/products/:id', validatorHandler(getOne, 'params'), getProduct);
   app.patch('/products/:id', validatorHandler(updateProduct, 'body'), updateProductCon);
 };
