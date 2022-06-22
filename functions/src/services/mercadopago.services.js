2090d74fbd32ccbbdcd5912ddbf5c04eaa2584b7
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-06-22 18:24:04 -0300

    loik

--- functions/src/services/mercadopago.services.js
+++ functions/src/services/mercadopago.services.js
@@ -4,35 +4,6 @@ const boom = require('@hapi/boom');
 const axios = require('axios');
 
 class Mercadopago {
-  async createPlan(reason, amount, frequency) {
-    if (!reason || !amount || !frequency) {
-      throw boom.badRequest('missing required fields');
-    }
-
-    const body = {
-      back_url: config.backUrlMp,
-      reason: reason,
-      auto_recurring: {
-        frequency: frequency,
-        frequency_type: 'months',
-        transaction_amount: amount,
-        currency_id: 'ARS',
-      },
-    };
-
-    try {
-      let result = await axios.post(config.urlConsultaPagos, body, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ` + config.tokenMP,
-        },
-      });
-      return result.data;
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
   async consultSubscription(id) {
     if (!id) {
       throw boom.badRequest('missing required fields');
