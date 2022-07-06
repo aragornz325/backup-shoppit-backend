03e2c55eadca31065fba7aed28a595109d0f43da
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-07-06 10:11:46 -0300

    integrada busqueda libre con algolia

--- functions/src/utils/free.search.algolia.js
+++ functions/src/utils/free.search.algolia.js
@@ -1,15 +1,13 @@
 const algoliasearch = require('algoliasearch');
 const { config } = require('../config/config');
 const functions = require('firebase-functions');
-const { db } = require('../../config/firebase');
+const UserRepository = require('../repositories/user.repository');
+const service = new UserRepository();
 const client = algoliasearch(
   `${config.algoliaAppId}`,
   `${config.algoliaApiSearch}`
 );
 const index = client.initIndex(`${config.algoliaIndexName}`);
-const UserRepository = require('../repositories/user.repository');
-const service = new UserRepository();
-const boom = require('@hapi/boom');
 
 async function freeSearch(query) {
   functions.logger.info('execute search');
@@ -28,13 +26,8 @@ async function freeSearch(query) {
   });
 
   for (let i = 0; i < searchID.length; i++) {
-    const userRef = db.collection('users').doc(searchID[i]);
-    const user = await userRef.get();
-    if (!user.exists) {
-      functions.logger.error(`user with ID ${searchID[i]} not found`);
-      throw boom.badData(`user with ID ${searchID[i]} not found`);
-    }
-    result.push(user.data());
+    const user = await service.getUserById(searchID[i]);
+    result.push(user);
   }
 
   return result;
