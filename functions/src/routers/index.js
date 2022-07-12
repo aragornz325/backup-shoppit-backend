22da75b2d63e2d23d7ca2632d945f8cc0b9a2258
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-07-12 12:58:21 -0300

    :construction: workin on update from sheet

--- functions/src/routers/index.js
+++ functions/src/routers/index.js
@@ -1,9 +1,11 @@
 const useRouter = require('./user.routes');
 const productsRouter = require('./products.router');
+const membershipsRouter = require('./memberships.router');
 
 function routerApi(app) {
   app.use('/users', useRouter);
   app.use('/products', productsRouter);
+  app.use('/memeberships', membershipsRouter);
 }
 
 module.exports = routerApi;
