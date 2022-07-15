16bf05987f35c7e5727da3d0a61ab966facf1f81
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-07-15 16:17:25 -0300

    :construction: seting SKU

---
+++ functions/src/routers/products.routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const PrductsController = require('../controllers/products.controller');
+const productsController = new PrductsController();
+const {
+  createProduct,
+  updateProduct,
+  getOne,
+} = require('../schemas/prod.schema');
+const {
+  isAuthenticated,
+  isAuthorized,
+  checkApiKey,
+} = require('../middlewares/auth.handler');
+
+const validatorHandler = require('../middlewares/validatorHandler');
+
+router.get('', productsController.getProducts);
+router.post(
+  '/:id',
+  validatorHandler(createProduct, 'body'),
+  productsController.createProduct
+);
+router.get('/:id', productsController.getProductById);
+router.put('/:id', productsController.updateProduct);
+router.delete('/:id', productsController.deleteProduct);
+
+module.exports = router;
--- functions/src/schemas/prod.schema.js
+++ functions/src/schemas/prod.schema.js
@@ -4,42 +4,59 @@ const isAstroselling = joi.boolean();
 const channel_id = joi.number().integer();
 const available_colors = joi.string();
 const available_size = joi.string();
-const categories = joi.string();
+const category = joi.string().valid('clothes', 'shoes', 'accessories', 'tech');
+const color = joi.string().max(20);
 const currency = joi.string().min(3).max(3).default('ARS');
 const stock = joi.number().integer();
-const description = joi.string();
+const description = joi.string().max(255);
+
 const featured = joi.boolean();
 const featuredImage = joi.string().uri();
 const freeShipping = joi.boolean();
 const height = joi.number();
 const id = joi.string().alphanum(); //TODO: requiere confirmacion del largo del ID
 const images = joi.array().items(joi.string().uri());
+const images_url = joi.array().items(joi.string().uri());
 const in_stock = joi.boolean();
 const is_published = joi.boolean();
 const longitude = joi.number();
 const manage_colors = joi.boolean();
 const manage_size = joi.boolean();
 const manage_stock = joi.boolean();
+const min_sell_amount = joi.number().integer();
 const name = joi.string().max(40);
 const origin = joi.string().max(20);
+const offer_price = joi.number().positive();
 const permalink = joi.string().uri();
 const plataform = joi.string().max(5);
 const price = joi.any();
 const productAttributes = joi.string();
+const publish = joi.boolean();
+const quantity = joi.number().integer();
 const quantity_to_cart = joi.string();
 const regular_price = joi.number().integer().positive();
 const sale_price = joi.number().integer().positive();
 const selected_variation = joi.string();
+const size = joi.string().valid('XS', 'S', 'M', 'L', 'XL', 'XXL');
 const sku = joi.number();
+const state = joi.string().valid('new', 'used');
 const status = joi.string();
 const stock_quantity = joi.number().positive();
 const short_description = joi.string().max(250);
 const tags = joi.array();
 const total_sales = joi.number();
+const total_stock = joi.number().positive();
 const type = joi.string();
 const updated = joi.array();
 const variable_products = joi.array();
-const variations = joi.any();
+const variation_type = joi.string().valid('tech', 'clotehs');
+
+const variations = joi.array().items({
+  variation_type,
+  size,
+  color,
+  quantity,
+});
 const vendor = joi.object({
   minimum_purchase: joi.number(),
   name: joi.string(),
@@ -51,100 +68,43 @@ const volumen = joi.number();
 const weight = joi.number();
 const width = joi.number();
 const withError = joi.boolean();
+const dimensions = joi.object({
+  height,
+  width,
+  longitude,
+  weight,
+});
 
 const createProduct = joi.object({
-  isAstroselling: isAstroselling.required(),
-  channel_id: channel_id,
-  available_colors: available_colors.required(),
-  available_size: available_size.required(),
-  categories: categories.required(),
-  currency: currency.required(),
-  description: description.required(),
-  featured: featured.required(),
-  featuredImage: featuredImage.required(),
-  freeShipping: freeShipping.required(),
-  height: height.required(),
-  //id: id.required(),
-  images: images.required(),
-  in_stock: in_stock.required(),
-  is_published: is_published.required(),
-  longitude: longitude.required(),
-  manage_colors: manage_colors.required(),
-  manage_size: manage_size.required(),
-  manage_stock: manage_stock.required(),
   name: name.required(),
-  origin: origin.required(),
-  permalink: permalink.required(),
-  plataform: plataform.required(),
-  price: price.required(),
-  productAttributes: productAttributes.required(),
-  quantity_to_cart: quantity_to_cart.required(),
+  description: description.required(),
   regular_price: regular_price.required(),
-  sale_price: sale_price.required(),
-  selected_variation: selected_variation.required(),
+  state: state.required(),
+  variations: variations.required(),
+  images_url: images_url.required(),
+  total_stock: total_stock.required(),
   sku: sku.required(),
-  status: status.required(),
-  stock_quantity: stock_quantity.required(),
-  short_description: short_description.required(),
-  tags: tags.required(),
-  total_sales: total_sales.required(),
-  type: type.required(),
-  updated: updated.required(),
-  variable_products: variable_products.required(),
-  variations: variations,
-  vendor: vendor.required(),
-  volumen: volumen.required(),
-  weight: weight.required(),
-  width: width.required(),
-  withError: withError.required(),
+  category: category.required(),
+  publish: publish.required(),
+  offer_price: offer_price.required(),
+  min_sell_amount: min_sell_amount.required(),
+  dimensions: dimensions.required(),
 });
 
 const updateProduct = joi.object({
-  isAstroselling,
-  channel_id,
-  available_colors,
-  available_size,
-  categories,
-  currency,
-  description,
-  featured,
-  featuredImage,
-  freeShipping,
-  height,
-  id,
-  images,
-  in_stock,
-  is_published,
-  longitude,
-  manage_colors,
-  manage_size,
-  manage_stock,
   name,
-  origin,
-  permalink,
-  plataform,
-  price,
-  productAttributes,
-  quantity_to_cart,
+  description,
   regular_price,
-  sale_price,
-  selected_variation,
-  sku,
-  stock,
-  status,
-  stock_quantity,
-  short_description,
-  tags,
-  total_sales,
-  type,
-  updated,
-  variable_products,
+  state,
   variations,
-  vendor,
-  volumen,
-  weight,
-  width,
-  withError,
+  images_url,
+  total_stock,
+  sku,
+  category,
+  publish,
+  offer_price,
+  min_sell_amount,
+  dimensions,
 });
 
 const getOne = joi.object({
