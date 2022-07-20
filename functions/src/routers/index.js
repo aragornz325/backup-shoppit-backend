c8a1211127ea9e775e09209ae8806845a2ef9557
Aragornz <70964354+aragornz325@users.noreply.github.com>
2022-07-20 10:49:41 -0300

    update branch

--- functions/src/routers/index.js
+++ functions/src/routers/index.js
@@ -1,13 +1,11 @@
 const useRouter = require('./user.routes');
 const productsRouter = require('./products.router');
 const membershipsRouter = require('./memberships.routes');
-const sheet2 = require('./sheet2.routes');
 
 function routerApi(app) {
   app.use('/users', useRouter);
   app.use('/products', productsRouter);
   app.use('/memberships', membershipsRouter);
-  app.use('/sheet2', sheet2);
 }
 
 module.exports = routerApi;
--- functions/src/routers/sheet2.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Sheet2Controller = require('../controllers/sheet2.controller');
-const sheet2Controller = new Sheet2Controller();
-
-router.get('/:id', sheet2Controller.getSheet2);
-
-module.exports = router;
-//#endregion
