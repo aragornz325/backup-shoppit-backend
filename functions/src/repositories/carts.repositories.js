c2cc8d168b7a23bfa16387830a7c9a7709b749e3
Aragornz <rodrigo.m.quintero@gmail.com>
2022-08-23 15:21:53 -0300

    crud carts

--- functions/src/repositories/carts.repositories.js
+++ functions/src/repositories/carts.repositories.js
@@ -4,32 +4,7 @@ const boom = require('@hapi/boom');
 
 class CartsRepository {
   async createCart(payload) {
-    let total_quantity = 0;
-    if (payload.products_list.length == 1) {
-      total_quantity = payload.products_list[0].quantity;
-    } else {
-      for (let i = 0; i < payload.products_list.length; i++) {
-        total_quantity += payload.products_list[i].quantity;
-      }
-    }
-    let amount = 0;
-    for (let i = 0; i < payload.products_list.length; i++) {
-      const prioductDb = await db
-        .collection('products')
-        .doc(payload.products_list[i].product_id)
-        .get();
-      amount +=
-        prioductDb.data().regular_price * payload.products_list[i].quantity;
-    }
-
-    const cart = {
-      owner_id: payload.owner_id,
-      products_list: payload.products_list,
-      total_price: amount,
-      total_quantity: total_quantity,
-    };
-
-    await db.collection('carts').add(cart);
+    await db.collection('carts').add(payload);
     return { msg: 'ok' };
   }
 
--- functions/src/services/carts.services.js
+++ functions/src/services/carts.services.js
@@ -1,14 +1,42 @@
 const CartsRepository = require('../repositories/carts.repositories');
 const cartsRepository = new CartsRepository();
+const { db } = require('../../config/firebase');
 
 class CartsServices {
   async createCart(payload) {
-    return await cartsRepository.createCart(payload);
+    let total_quantity = 0;
+    if (payload.products_list.length == 1) {
+      total_quantity = payload.products_list[0].quantity;
+    } else {
+      for (let i = 0; i < payload.products_list.length; i++) {
+        total_quantity += payload.products_list[i].quantity;
+      }
+    }
+    let amount = 0;
+    for (let i = 0; i < payload.products_list.length; i++) {
+      const prioductDb = await db
+        .collection('products')
+        .doc(payload.products_list[i].product_id)
+        .get();
+      amount +=
+        prioductDb.data().regular_price * payload.products_list[i].quantity;
+    }
+
+    const cart = {
+      owner_id: payload.owner_id,
+      products_list: payload.products_list,
+      total_price: amount,
+      total_quantity: total_quantity,
+    };
+
+    return await cartsRepository.createCart(cart);
   }
 
   async getAllCarts() {
     return await cartsRepository.getAllCarts();
   }
+
+  async updateCart(payload) {}
 }
 
 module.exports = CartsServices;
