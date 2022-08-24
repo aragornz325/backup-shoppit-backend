b01ddc9a1064375bace337c653c124a0a49dfcaf
Aragornz <rodrigo.m.quintero@gmail.com>
2022-08-24 14:34:00 -0300

    cart crud

--- functions/src/routers/carts.routes.js
+++ functions/src/routers/carts.routes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const CartsController = require('../controllers/carts.controller');
 const cartsController = new CartsController();
+const { cartSchema } = require('../schemas/carts.schema');
+const validatorHandler = require('../middlewares/validatorHandler');
 
-router.post('', cartsController.createCart);
+router.post(
+  '',
+  validatorHandler(cartSchema, 'body'),
+  cartsController.createCart
+);
 router.get('', cartsController.getAllCarts);
 router.get('/:cart_id', cartsController.getCartById);
 router.patch('/:cart_id', cartsController.updateCart);
