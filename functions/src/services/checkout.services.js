0249930a5c915e01bf362263abb7ed7b9a414f96
Aragornz <rodrigo.m.quintero@gmail.com>
2022-09-03 11:53:48 -0300

    create orders

--- functions/src/services/checkout.services.js
+++ functions/src/services/checkout.services.js
@@ -10,63 +10,6 @@ const cartsRepository = new CartsRepository();
 const functions = require('firebase-functions');
 
 class CheckoutServices {
-  async createOrder1(order) {
-    // check if user exists
-    await userRepository.getUserById(order.owner_id);
-
-    // calculate total quantity
-    let total_quantity = 0;
-    for (let i = 0; i < order.products_list.length; i++) {
-      total_quantity += order.products_list[i].quantity;
-    }
-
-    // calculate total amount
-    let amount = 0;
-    for (let i = 0; i < order.products_list.length; i++) {
-      const prioductDb = await productsRepository.getProductById(
-        order.products_list[i].product_id
-      );
-      amount += prioductDb[0].regular_price * order.products_list[i].quantity;
-    }
-
-    // create order
-    const order_items = order.products_list.map((product) => {
-      return {
-        product_id: product.product_id,
-        quantity: product.quantity,
-        status_by_seller: 'pending',
-        status_by_buyer: 'approved',
-      };
-    });
-
-    const orderToDb = {
-      owner_id: order.owner_id,
-      order_items,
-      total_price: amount,
-      total_quantity: total_quantity,
-      created_at: Math.floor(Date.now() / 1000),
-      status: 'pending',
-    };
-
-    //save order to db
-    await checkoutRepository.createOrder(orderToDb);
-
-    //set user cart to empty
-    const cart = {
-      owner_id: order.owner_id,
-      products_list: [],
-      total_quantity: 0,
-      total_price: 0,
-    };
-    const check_cart = await cartsRepository.getCartByOwner(order.owner_id);
-    if (check_cart.length <= 0) {
-      await cartsRepository.createCart(cart);
-    } else {
-      await cartsRepository.setCart(cart, check_cart[0].id, false);
-    }
-    return { msg: 'ok' };
-  }
-
   async createOrder(order) {
     const productsByIdMap = new Map();
     for (let i = 0; i < order.products_list.length; i++) {
